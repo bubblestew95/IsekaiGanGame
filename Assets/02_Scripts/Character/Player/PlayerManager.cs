9bfcace8ee21d4f0045768ed3fcd6d438b8cd5b1
DESKTOP-HFCF82F\admin <bubblestew95@gmail.com>
2025-01-23 10:45:57 +0900

    가상 조이스틱에 대응하여 캐릭터 움직임 구현 완료.

--- Assets/02_Scripts/Character/Player/PlayerManager.cs
+++ Assets/02_Scripts/Character/Player/PlayerManager.cs
@@ -5,5 +5,31 @@ using UnityEngine;
 /// </summary>
 public class PlayerManager : MonoBehaviour
 {
-    
+    [SerializeField]
+    private FloatingJoystick joystick = null;
+
+    [SerializeField]
+    private float speed = 1f;
+
+    private CharacterController characterCont = null;
+
+    private void Awake()
+    {
+        characterCont = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        float x = joystick.Horizontal;
+        float z = joystick.Vertical;
+
+        Vector3 moveVector = new Vector3(x, 0f, z) * speed * Time.deltaTime;
+
+        characterCont.Move(moveVector);
+
+        if (moveVector.sqrMagnitude == 0f)
+            return;
+
+        transform.rotation = Quaternion.LookRotation(moveVector);
+    }
 }
