dc1ded46985877f9657bea6efb1b0ef00503625e
jhdsew9471 <jhdsew9471@gmail.com>
2025-01-24 14:11:20 +0900

    캐릭터 전투 UI 설정 완료

--- Assets/02_Scripts/UI/UIBattle/Button/CoolTime.cs
+++ Assets/02_Scripts/UI/UIBattle/Button/CoolTime.cs
@@ -8,7 +8,8 @@ public class CoolTime : MonoBehaviour
     public float maxCooltime = 0;
     public float curCooltime = 0;
     private Image coolImage = null;
-    private TextMeshProUGUI coolSecondUI = null;
+    public TextMeshProUGUI coolSecondUI = null;
+    public bool isPressed = false;
     private void Awake()
     {
         coolSecondUI = GetComponentInChildren<TextMeshProUGUI>();
@@ -18,6 +19,7 @@ public class CoolTime : MonoBehaviour
     public void SetMaxCooltime(float _cooltime)
     {
         maxCooltime = _cooltime;
+        CooltimeDown();
     } // 초기 쿨타임 설정
 
     public void CooltimeDown()
@@ -26,17 +28,28 @@ public class CoolTime : MonoBehaviour
         StartCoroutine(cooldown());
     } // 함수 실행하면 쿨이 돈다
 
-
     private IEnumerator cooldown() // UI 표시랑 쿨 계산 동시에 하는 코루틴
     {
         float coolratio = 0;
-        while (curCooltime<=0)
+        coolSecondUI.enabled = true;
+        isPressed = true;
+        while (curCooltime > 0)
         {
             curCooltime -= Time.deltaTime; //쿨타임 계산
-            coolSecondUI.text = curCooltime.ToString(); //쿨타임UI Txt표시
+            if (curCooltime >= 1)
+            {
+                coolSecondUI.text = curCooltime.ToString("F0"); //쿨타임UI Txt표시
+            }
+            if (curCooltime < 1)
+            {
+                coolSecondUI.text = curCooltime.ToString("F1"); //쿨타임UI Txt표시
+            }
             coolratio = curCooltime / maxCooltime; // 현재쿨 비율 계산
             coolImage.fillAmount = coolratio;//쿨타임UI 시계표시
             yield return null;
         }
+        curCooltime = 0; 
+        coolSecondUI.enabled = false;
+        isPressed = false;
     }
 }
--- Assets/02_Scripts/UI/UIBattle/UIBattleUIManager.cs
+++ Assets/02_Scripts/UI/UIBattle/UIBattleUIManager.cs
@@ -5,15 +5,62 @@ using System.Linq;
 public class UIBattleUIManager : MonoBehaviour
 {
     public List<UIHpsManager> hpList = new List<UIHpsManager>();
+    public List<UIBossHpsManager> bossHpList = new List<UIBossHpsManager>();
     public List<UIWarningManager> warningList = new List<UIWarningManager>();
     public List <ButtonSetting> buttonList = new List<ButtonSetting>();
     public FloatingJoystick joyStick = null;
+    public List<float> cooltimeList = new List<float>(); //평타,회피,스킬1,스킬2,스킬3 순서
+
+    public int bossMaxHp = 1;
+    public int playerMaxHp = 1;
 
     private void Awake()
     {
         hpList = GetComponentsInChildren<UIHpsManager>().ToList();
+        bossHpList = GetComponentsInChildren<UIBossHpsManager>().ToList();
         warningList = GetComponentsInChildren<UIWarningManager>().ToList();
         buttonList = GetComponentsInChildren<ButtonSetting>().ToList();
         joyStick = GetComponentInChildren<FloatingJoystick>();
+        //테스트 용 쿨타임
+        if (cooltimeList.Count <= 4)
+        {
+            List<float> testCoolValue = new List<float> { 1, 1, 1, 1, 1 };
+            cooltimeList = testCoolValue;
+            if (cooltimeList.Count ==0)
+                Debug.Log("쿨타임 리스트가 비어있어 테스트 쿨타임으로 시작합니다.");            
+            if (cooltimeList.Count < 0)
+                Debug.Log("쿨타임 리스트가 5개보다 적어 테스트 쿨타임으로 시작합니다.");
+        }
+        //
+        SetupAllUI();
+    }
+
+    public void CooltimeListSetting(List<float> _timeList) // 평타,회피,스킬1,스킬2,스킬3 순서
+    {
+        for (int i = 0; i < _timeList.Count; i++)
+        {
+            cooltimeList[i] = _timeList[i];
+        }
+    }
+
+    public void SetupAllUI() // 전체적으로 한번 싹 정하고 시작
+    {
+        foreach (UIBossHpsManager hp in bossHpList) // 보스 최대체력 설정
+        {
+            hp.SetMaxHp(bossMaxHp);
+            hp.HpBarUIUpdate();
+        }        
+        foreach (UIHpsManager hp in hpList) // 플레이어 최대체력 설정
+        {
+            hp.SetMaxHp(playerMaxHp);
+            hp.HpBarUIUpdate();
+        }
+        warningList[0].ReConnection(); // 연결 오류 UI 비활성화
+
+        for (int i = 0; i < buttonList.Count; i++) // 쿨타임 리스트에 있는 쿨타임들 각각 버튼에 설정// 평타,회피,스킬1,스킬2,스킬3 순서
+        {
+            ButtonSetting buttonSetting = buttonList[i];
+            buttonSetting.SetCooltime(cooltimeList[i]);
+        }
     }
 }
