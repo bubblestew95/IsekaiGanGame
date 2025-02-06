b7d274ec69949664d7adbd26d005bb9902b3f3c9
DESKTOP-HFCF82F\admin <bubblestew95@gmail.com>
2025-02-06 16:26:16 +0900

    어쌔신 애니메이션 제작중

--- Assets/02_Scripts/ScriptableData/SkillDatas/Attack/RangeHoldingSkill.cs
+++ Assets/02_Scripts/ScriptableData/SkillDatas/Attack/RangeHoldingSkill.cs
@@ -2,45 +2,18 @@ using System.Collections;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "RangeHoldingSkill", menuName = "Scriptable Objects/Player Skill/RangeHolding")]
-public class RangeHoldingSkill : AttackSkill
+public class RangeHoldingSkill : RangeSkill
 {
     [Header("Range Holding Skill")]
     public float duration = 2f;
-    public float damageTickTime = 0.5f;
-    public float maxDistance = 5f;
 
-    public override void UseSkill(PlayerManager _player)
+    public override void StartSkill(PlayerManager _player)
     {
-        base.UseSkill(_player);
-
-        _player.StartCoroutine(SkillHoldingCoroutine(_player));
+        base.StartSkill(_player);
     }
 
     public override void EndSkill(PlayerManager _player)
     {
         base.EndSkill(_player);
-    }
-
-    private IEnumerator SkillHoldingCoroutine(PlayerManager _player)
-    {
-        float currentTime = 0f;
-        WaitForSeconds waitSec = new WaitForSeconds(damageTickTime);
-
-        Vector3 direction = _player.LastSkillUsePoint;
-
-        Ray ray = new Ray(_player.RangeAttackStartTr.position, direction);
-        RaycastHit hit;
-
-        while (currentTime <= duration)
-        {
-            if(Physics.Raycast(ray, out hit, maxDistance))
-            {
-                _player.AddDamageToBoss(DamageCalculate(_player), aggro);
-            }
-
-            yield return waitSec;
-
-            currentTime += damageTickTime;
-        }
     }
 }
