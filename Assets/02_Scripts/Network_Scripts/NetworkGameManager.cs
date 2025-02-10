ee18d4c721fbd599c7bbb989204fe7c1e93eca36
Jeon\admin <bubblestew95@gmail.com>
2025-02-10 19:12:41 +0900

    workwork

--- Assets/02_Scripts/Network_Scripts/NetworkGameManager.cs
+++ Assets/02_Scripts/Network_Scripts/NetworkGameManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class NetworkGameManager : NetworkManager
+public class NetworkGameManager : NetworkBehaviour
 {
     public event Action loadingFinishCallback;
 
