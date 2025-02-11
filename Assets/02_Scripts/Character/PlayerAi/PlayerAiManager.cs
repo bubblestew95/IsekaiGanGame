a5a498973d36f1d7832199d8c792326e73bec6d1
jhdsew9471 <jhdsew9471@gmail.com>
2025-02-11 11:10:29 +0900

    플레이어 AI 보완

--- Assets/02_Scripts/Character/PlayerAi/PlayerAiManager.cs
+++ Assets/02_Scripts/Character/PlayerAi/PlayerAiManager.cs
@@ -12,6 +12,16 @@ public class PlayerAiManager : MonoBehaviour
     public Vector3 mapCenterPos; // 맵의 중앙 위치 
     public float rockSize = 2.0f; // 돌의 크기 (예시로 설정, 실제 돌 크기에 맞게 조정)
     WaitForSeconds wait = new WaitForSeconds(0.5f);
+    float safeDistance = 2f;
+    // SkillSlot 배열에 사용할 스킬을 나열
+    SkillSlot[] skillSlots = new SkillSlot[]
+    {
+        SkillSlot.Skill_A,
+        SkillSlot.Skill_B,
+        SkillSlot.Skill_C,
+        SkillSlot.BasicAttack
+    };
+
     private void Awake()
     {
         playerManager = GetComponent<PlayerManager>();
@@ -27,19 +37,31 @@ public class PlayerAiManager : MonoBehaviour
         {
             rocksPos.Add(rock.transform.position); // 돌의 위치 추가
         }
-    } 
+    }
     #endregion
     #region 이동
     #region 기본 이동
     private void AiMove(float _x, float _z) // 플레이어 매니저에 조이스틱 입력이랑 같은 입력을 줘서 움직이는 함수
     {
         JoystickInputData data = new JoystickInputData();
-        if (_x>1 || _x<-1 || _z > 1 || _z < -1)
+
+        // 입력 값이 유효 범위를 벗어났을 경우
+        if (_x > 1 || _x < -1 || _z > 1 || _z < -1)
         {
-            Debug.Log("Ai move input is out of range, Range is (-1 ~ 1) (X:" + _x + ",Z:" + _z + ")");
+            Debug.Log("Ai move input is out of range, Range is (-1 ~ 1) (X:" + _x + ", Z:" + _z + ")");
+
+            // 입력 값을 -1 또는 1로 고정 (음수면 -1, 양수면 1)
+            data.x = Mathf.Sign(_x); // _x가 음수면 -1, 양수면 1
+            data.z = Mathf.Sign(_z); // _z가 음수면 -1, 양수면 1
         }
-        data.x = _x;
-        data.z = _z;
+        else
+        {
+            // 입력 값이 유효 범위 내에 있을 경우 그대로 사용
+            data.x = _x;
+            data.z = _z;
+        }
+
+        // 조이스틱 데이터를 이용해 이동 처리
         playerManager.MovementManager.MoveByJoystick(data);
     }
     #endregion
@@ -82,14 +104,27 @@ public class PlayerAiManager : MonoBehaviour
         //플레이어 위치
         //코루틴 이동 범위 밖으로 가는
     }
-    private void GoToBossForAttack() // 공격하기 위해 보스에게 가까이 가는 함수 ( 사유 공격 
+    private void GoToBossForAttack(float _safeDistance) // 공격하기 위해 보스에게 가까이 가거나 멀어지는 함수 ( 사유 공격 )
     {
-        Vector3 bossPos = playerManager.InputManager.lastSkillUsePoint; // 보스 위치
-        Vector3 playerPos = transform.position;//플레이어 위치 받기 playerPos
-        // 목표 위치를 계산한 후, 목표로 향할 방향 (AI 이동)
+        Vector3 bossPos = GameManager.Instance.GetBossTransform().position;
+        Vector3 playerPos = transform.position; // 플레이어 위치 받기
+
+        // 보스와 플레이어의 방향 벡터를 계산
         Vector3 directionToTarget = bossPos - playerPos;
+        float distanceToBoss = directionToTarget.magnitude; // 보스와의 거리 계산
+
+        // 목표 위치에서 너무 가까워지지 않도록 일정 거리만큼 떨어져서 이동
+
+        if (distanceToBoss < _safeDistance)
+        {
+            // 너무 가까운 경우, 보스에서 일정 거리만큼 떨어지도록 방향을 조정
+            directionToTarget = directionToTarget.normalized * (distanceToBoss - _safeDistance);
+        }
+
+        // 방향 벡터를 x, z 좌표로 변환하여 AiMove 함수에 전달
         float x = directionToTarget.x;
         float z = directionToTarget.z;
+
         AiMove(x, z);
     }
     #endregion
@@ -97,33 +132,35 @@ public class PlayerAiManager : MonoBehaviour
     #region 공격
     private void AiUseSkillWithAim(SkillSlot _type) // 스킬 데이터를 받아 사거리내에 보스가 있다면 보스 위치에 스킬쓰고 아니면 보스에게 가는 함수
     {
-        Vector3 bossPos = playerManager.InputManager.lastSkillUsePoint; // 보스 위치
+        Vector3 bossPos = GameManager.Instance.GetBossTransform().position; // 보스 위치
         Vector3 playerPos = transform.position;//플레이어 위치 받기 playerPos
         float bossPlayerDistance = Vector3.Distance(playerPos, bossPos); // 보스랑 플레이어 위치 계산
 
         RangeSkill rangeSkill = playerManager.SkillManager.GetSkillData(_type) as RangeSkill; // 스킬 데이터를 받아옴
         if(rangeSkill == null) // 범위 스킬이 아니면 (근접공격)
         {
-            if (bossPlayerDistance < 1f) // 보스와 플레이어 사이의 거리가 1미만이라면
+            if (bossPlayerDistance < safeDistance) // 보스와 플레이어 사이의 거리가 safeDistance미만이라면
             {
+                playerManager.InputManager.lastSkillUsePoint = bossPos; // 스킬 써야하는 위치 
                 playerManager.SkillManager.TryUseSkill(_type, bossPos); // 보스위치에 스킬 사용
             }
             else
             {
                 Debug.Log("Ai Log Boss is out of Skill range");
-                GoToBossForAttack();
+                GoToBossForAttack(safeDistance);
             }
         }
         else // 범위 스킬이면
         {
             if (bossPlayerDistance < rangeSkill.attackRange) // 스킬 사정거리가 보스와 플레이어 사이 거리보다 작으면
             {
+                playerManager.InputManager.lastSkillUsePoint = bossPos; // 스킬 써야하는 위치 
                 playerManager.SkillManager.TryUseSkill(_type, bossPos); // 보스위치에 스킬 사용
             }
             else
             {
                 Debug.Log("Ai Log Boss is out of Skill range");
-                GoToBossForAttack();
+                GoToBossForAttack(rangeSkill.attackRange);
             }
         }
     }
@@ -131,9 +168,10 @@ public class PlayerAiManager : MonoBehaviour
 
     private void Update()
     {
-        AiUseSkillWithAim(SkillSlot.Skill_A);
-        AiUseSkillWithAim(SkillSlot.Skill_B);
-        AiUseSkillWithAim(SkillSlot.Skill_C);
-        AiUseSkillWithAim(SkillSlot.BasicAttack);
+        // 배열을 순회하면서 각 스킬에 대해 AiUseSkillWithAim을 호출
+        foreach (SkillSlot skill in skillSlots)
+        {
+            AiUseSkillWithAim(skill);
+        }
     }
 }
