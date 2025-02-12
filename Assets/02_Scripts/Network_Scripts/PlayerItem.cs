4057f23e92a24aad984479fc992821d110e4a6b5
DESKTOP-PKN30ES\user <ljhunity@gmail.com>
2025-02-12 13:04:19 +0900

    플레이어 List 제작중

--- Assets/02_Scripts/Network_Scripts/PlayerItem.cs
+++ Assets/02_Scripts/Network_Scripts/PlayerItem.cs
@@ -1,16 +1,15 @@
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
 
-public enum PlayerStatus { Host, Ready, NotReady }
+public enum PlayerStatus { Host, Ready, Not_Ready }
 
 public class PlayerItem : MonoBehaviour
 {
     public TMP_Text usernameText;
     public Image statusIcon;
-    public Sprite hostIcon;
-    public Sprite readyIcon;
-    public Sprite notReadyIcon;
 
 
     public void SetPlayerInfo(string username, PlayerStatus status)
@@ -21,23 +20,41 @@ public class PlayerItem : MonoBehaviour
 
     public void SetStatus(PlayerStatus status)
     {
-        switch (status)
+        string labelKey = "LobbyPlayerIcons"; // 지정한 Label을 사용
+
+        Addressables.LoadAssetsAsync<Sprite>(labelKey, null).Completed += (handle) =>
         {
-            case PlayerStatus.Host:
-                statusIcon.sprite = hostIcon;
-                break;
-            case PlayerStatus.Ready:
-                statusIcon.sprite = readyIcon;
-                break;
-            case PlayerStatus.NotReady:
-                statusIcon.sprite = notReadyIcon;
-                break;
-        }
+            if (handle.Status == AsyncOperationStatus.Succeeded)
+            {
+                foreach (var sprite in handle.Result)
+                {
+                    if (sprite.name.Contains(status.ToString()))
+                    {
+                        statusIcon.sprite = sprite;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError($"[PlayerItem] {labelKey}에서 아이콘을 찾을 수 없습니다!");
+            }
+        };
     }
 
-    public void SetReadyState(bool isReady)
+    public void SetReadyState(bool isReady, bool isHost = false)
     {
-        //readyIndicator.SetActive(isReady);
+        if (isHost)
+        {
+            statusIcon.sprite = Resources.Load<Sprite>("05_UI/Images/Lobby_Player/HostIcon");
+        }
+        else
+        {
+            string spritePath = isReady ? "05_UI/Images/Lobby_Player/ReadyIcon" : "05_UI/Images/Lobby_Player/Not_ReadyIcon";
+            statusIcon.sprite = Resources.Load<Sprite>(spritePath);
+        }
+
+        Debug.Log($"[PlayerItem] Ready 상태 변경 - 플레이어: {usernameText.text}, 상태: {(isReady ? "Ready" : "Not Ready")}");
     }
 
 }
