d9b9b3d95c755b73eea3195b588f7f645fa869ff
jhdsew9471 <jhdsew9471@gmail.com>
2025-02-13 17:08:55 +0900

    로그인UI 개편

--- Assets/02_Scripts/UI/FillScreenImage.cs
+++ Assets/02_Scripts/UI/FillScreenImage.cs
@@ -3,16 +3,37 @@ using UnityEngine.UI;
 
 public class FillScreenImage : MonoBehaviour
 {
+    RectTransform rectTransform;
+    Vector2 lastScreenSize;
+
     void Start()
     {
+        // RectTransform 초기화
         Image image = GetComponent<Image>();
-        RectTransform rectTransform = image.GetComponent<RectTransform>();
+        rectTransform = image.GetComponent<RectTransform>();
 
-        // 화면 크기 받아오기
-        float screenWidth = Screen.width;
-        float screenHeight = Screen.height;
+        // 처음 화면 크기 설정
+        lastScreenSize = new Vector2(Screen.width, Screen.height);
 
-        // RectTransform 크기 설정
-        rectTransform.sizeDelta = new Vector2(screenWidth, screenHeight);
+        // 초기 사이즈 설정
+        UpdateSize();
+    }
+
+    void Update()
+    {
+        // 화면 크기 변동이 있을 때만 갱신
+        Vector2 currentScreenSize = new Vector2(Screen.width, Screen.height);
+
+        if (currentScreenSize != lastScreenSize)
+        {
+            lastScreenSize = currentScreenSize;
+            UpdateSize();
+        }
+    }
+
+    void UpdateSize()
+    {
+        // 화면 크기 기반으로 이미지 크기 업데이트
+        rectTransform.sizeDelta = new Vector2(Screen.width, Screen.height);
     }
 }
