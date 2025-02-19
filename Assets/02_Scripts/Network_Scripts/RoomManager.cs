269bfd372ac1b3ee4578ecaf038d2363f461bddb
DESKTOP-G0M1PEK\user <ljhunity@gmail.com>
2025-02-19 16:45:40 +0900

    디폴트 플레이어명(로비) - Player

--- Assets/02_Scripts/Network_Scripts/RoomManager.cs
+++ Assets/02_Scripts/Network_Scripts/RoomManager.cs
@@ -115,8 +115,8 @@ public class RoomManager : NetworkBehaviour
         if (string.IsNullOrEmpty(username))
         {
             //Debug.LogError("Username not found!");
-            username = "imsi";
-            Debug.Log($"imsi - Welcome : , {username}!");
+            username = "Player";
+            Debug.Log($"Player - Welcome : , {username}!");
         }
         else
         {
