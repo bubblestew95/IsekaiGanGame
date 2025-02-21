eaaf863803202fe06017e2146c52d5b1458c50cb
Jeon\admin <bubblestew95@gmail.com>
2025-02-21 16:16:21 +0900

    전투 로그 각 플레이어, 보스, 행동에 따른 이미지 생성

--- Assets/02_Scripts/UI/BattleLog.cs
+++ Assets/02_Scripts/UI/BattleLog.cs
@@ -1,14 +1,18 @@
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
+
+using StructTypes;
+using EnumTypes;
 
 public class BattleLog : MonoBehaviour
 {
+    [Header("UI")]
     /// <summary>
     /// 행동 이미지
     /// </summary>
     [SerializeField]
-    private LogSlot image_How = null;
+    private LogSlot image_Behaviour = null;
     /// <summary>
     /// 주체 이미지
     /// </summary>
@@ -19,21 +23,53 @@ public class BattleLog : MonoBehaviour
     /// </summary>
     [SerializeField]
     private LogSlot image_Whom = null;
-
     /// <summary>
     /// 로그 출력 지속시간
     /// </summary>
     [SerializeField]
     private float showLogDuration = 2f;
 
+    [Header("Data")]
+    [SerializeField]
+    private List<BossImageData> bossImageDataList = null;
+    [SerializeField]
+    private List<PlayerImageData> playerImageDataList = null;
+    [SerializeField]
+    private List<BehaviourImageData> behaviourImageDataList = null;
 
+    private Dictionary<BossType, Sprite> bossImageDic = null;
+    private Dictionary<CharacterClass, Sprite> CharacterImageDic = null;
+    private Dictionary<BehaviourLogType, Sprite> behaviourImageDic = null;
 
     /// <summary>
-    /// 로그의 이미지를 설정하는 함수.
+    /// 사망 로그를 출력하는 함수
     /// </summary>
-    public void SetLogImages()
+    public void SetKillLog(BossType _boss, CharacterClass _player, bool _isPlayerDied = true)
     {
+        if(_isPlayerDied)
+        {
+            image_Who.SetImage(bossImageDic[_boss]);
+            image_Whom.SetImage(CharacterImageDic[_player]);
+        }
+        else
+        {
+            image_Who.SetImage(CharacterImageDic[_player]);
+            image_Whom.SetImage(bossImageDic[_boss]);
+        }
 
+        image_Behaviour.SetImage(behaviourImageDic[BehaviourLogType.Kill]);
+    }
+
+    /// <summary>
+    /// 부활 로그를 출력하는 함수
+    /// </summary>
+    /// <param name="_revive"></param>
+    /// <param name="_getRevived"></param>
+    public void SetReviveLog(CharacterClass _revive, CharacterClass _getRevived)
+    {
+        image_Who.SetImage(CharacterImageDic[_revive]);
+        image_Whom.SetImage(CharacterImageDic[_getRevived]);
+        image_Behaviour.SetImage(behaviourImageDic[BehaviourLogType.Revive]);
     }
 
     /// <summary>
@@ -43,7 +79,7 @@ public class BattleLog : MonoBehaviour
     {
         // 로그를 출력한다.
         {
-            image_How.gameObject.SetActive(true);
+            image_Behaviour.gameObject.SetActive(true);
             image_Who.gameObject.SetActive(true);
             image_Whom.gameObject.SetActive(true);
         }
@@ -52,16 +88,35 @@ public class BattleLog : MonoBehaviour
         StartCoroutine(HideLogCoroutine(showLogDuration));
     }
 
-    //
     private IEnumerator HideLogCoroutine(float _duration)
     {
         yield return new WaitForSeconds(_duration);
 
         // 로그를 다시 숨긴다.
         {
-            image_How.gameObject.SetActive(false);
+            image_Behaviour.gameObject.SetActive(false);
             image_Who.gameObject.SetActive(false);
             image_Whom.gameObject.SetActive(false);
+        }
+    }
+
+    private void Awake()
+    {
+        bossImageDic = new Dictionary<BossType, Sprite>();
+        CharacterImageDic = new Dictionary<CharacterClass, Sprite>();
+        behaviourImageDic = new Dictionary<BehaviourLogType, Sprite>();
+
+        foreach (var data in bossImageDataList)
+        {
+            bossImageDic.Add(data.bossType, data.bossImage);
+        }
+        foreach (var data in playerImageDataList)
+        {
+            CharacterImageDic.Add(data.characterClass, data.playerImage);
+        }
+        foreach (var data in behaviourImageDataList)
+        {
+            behaviourImageDic.Add(data.behaviourType, data.behaviourImage);
         }
     }
 }
--- Assets/02_Scripts/UI/LogSlot.cs
+++ Assets/02_Scripts/UI/LogSlot.cs
@@ -5,6 +5,9 @@ public class LogSlot : MonoBehaviour
 {
     [SerializeField]
     private Image slotImage = null;
-    
 
+    public void SetImage(Sprite sprite)
+    {
+        slotImage.sprite = sprite;
+    }
 }
