eb06ce24844d9031b6409966b8d59c04c324b1ab
Jeon\admin <bubblestew95@gmail.com>
2025-02-25 09:30:10 +0900

    보스 아이콘 UI 대응되게 수정

--- Assets/02_Scripts/UI/UI_LogImgList.cs
+++ Assets/02_Scripts/UI/UI_LogImgList.cs
@@ -1,7 +1,25 @@
-using UnityEngine;
 using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class UI_LogImgList : MonoBehaviour
 {
     public List<Sprite> logImgList = null;
+
+    private void Start()
+    {
+        Image bossImage = GetComponent<Image>();
+
+        if (bossImage != null && GameManager.Instance != null)
+        {
+            if (GameManager.Instance.IsGolem)
+            {
+                bossImage.sprite = logImgList[0];
+            }
+            else if (GameManager.Instance.IsMush)
+            {
+                bossImage.sprite = logImgList[1];
+            }
+        }
+    }
 }
